c4abf3a0ca638d4de1ed3ba3806096c249eb0c04
girishgr8 <girishthatte35@gmail.com>
2023-02-11 22:48:35 +0530

    [+] SSL changes

--- server.js
+++ server.js
@@ -137,6 +137,6 @@ app.put("/api/updateUser/:id", (req, res) => {
 });
 
 const port = process.env.PORT || 4000;
-app.listen(port, "192.168.64.3", () =>
+app.listen(port, "127.0.0.1", () =>
   console.log(`Server started on port ${port}`)
 );
